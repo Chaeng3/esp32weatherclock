fa4b0811db8cfa16e5fa5e57ff0a0e0b845756d7
Chaeng <chaeng221@gmail.com>
2024-07-28 19:36:19 +0800

    Add files via upload

---
+++ ProvincesCities.h
@@ -0,0 +1,41 @@
+#ifndef PROVINCE_CITIES_H
+#define PROVINCE_CITIES_H
+
+const char* provincesCities[][30] = {
+    {"BeiJing"},
+    {"TianJin"},
+    {"ShangHai"},
+    {"ChongQing"},
+    {"HeBei", "ShiJiaZhuang", "TangShan", "QinHuangDao", "HanDan", "XingTai", "BaoDing", "ZhangJiaKou", "ChengDe", "CangZhou", "LangFang", "HengShui"},
+    {"ShanXi", "TaiYuan", "DaTong", "YangQuan", "ChangZhi", "JinCheng", "ShuoZhou", "JinZhong", "YunCheng", "XinZhou", "LinFen", "LvLiang"},
+    {"NeiMengGu", "HuHeHaoTe", "BaoTou", "WuHai", "ChiFeng", "TongLiao", "EErDuoSi", "HuLunBeiEr", "BaYanNaoEr", "WuLanChaBu"},
+    {"LiaoNing", "ShenYang", "DaLian", "AnShan", "FuShun", "BenXi", "DanDong", "JinZhou", "YingKou", "FuXin", "LiaoYang", "PanJin", "TieLing", "ChaoYang", "HuLuDao"},
+    {"JiLin", "ChangChun", "JiLin", "SiPing", "LiaoYuan", "TongHua", "BaiShan", "SongYuan", "BaiCheng", "YanBian"},
+    {"HeiLongJiang", "HaErBin", "QiQiHaEr", "JiXi", "HeGang", "ShuangYaShan", "DaQing", "YiChun", "JiaMuSi", "QiTaiHe", "MuDanJiang", "HeiHe", "SuiHua", "DaXingAnLing"},
+    {"JiangSu", "NanJing", "WuXi", "XuZhou", "ChangZhou", "SuZhou", "NanTong", "LianYunGang", "HuaiAn", "YanCheng", "YangZhou", "ZhenJiang", "TaiZhou", "SuQian"},
+    {"ZheJiang", "HangZhou", "NingBo", "WenZhou", "JiaXing", "HuZhou", "ShaoXing", "JinHua", "QuZhou", "ZhouShan", "TaiZhou", "LiShui"},
+    {"AnHui", "HeFei", "WuHu", "BengBu", "HuaiNan", "MaAnShan", "HuaiBei", "TongLing", "AnQing", "HuangShan", "ChuZhou", "FuYang", "SuZhou", "LuAn", "BoZhou", "ChiZhou", "XuanCheng"},
+    {"FuJian", "FuZhou", "XiaMen", "PuTian", "SanMing", "QuanZhou", "ZhangZhou", "NanPing", "LongYan", "NingDe"},
+    {"JiangXi", "NanChang", "JingDeZhen", "PingXiang", "JiuJiang", "XinYu", "YingTan", "GanZhou", "JiAn", "YiChun", "FuZhou", "ShangRao"},
+    {"ShanDong", "JiNan", "QingDao", "ZiBo", "ZaoZhuang", "DongYing", "YanTai", "WeiFang", "JiNing", "TaiAn", "WeiHai", "RiZhao", "LinYi", "DeZhou", "LiaoCheng", "BinZhou", "HeZe"},
+    {"HeNan", "ZhengZhou", "KaiFeng", "LuoYang", "PingDingShan", "AnYang", "HeBi", "XinXiang", "JiaoZuo", "PuYang", "XuChang", "LuoHe", "SanMenXia", "NanYang", "ShangQiu", "XinYang", "ZhouKou", "ZhuMaDian"},
+    {"HuBei", "WuHan", "HuangShi", "ShiYan", "YiChang", "XiangYang", "EZhou", "JingMen", "XiaoGan", "JingZhou", "HuangGang", "XianNing", "SuiZhou", "EnShi", "XianTao"},
+    {"HuNan", "ChangSha", "ZhuZhou", "XiangTan", "HengYang", "ShaoYang", "YueYang", "ChangDe", "ZhangJiaJie", "YiYang", "ChenZhou", "YongZhou", "HuaiHua", "LouDi"},
+    {"GuangDong", "GuangZhou", "ShenZhen", "ZhuHai",  "ShaoGuan","ShanTou", "FoShan", "JiangMen", "ZhanJiang", "MaoMing", "ZhaoQing", "HuiZhou", "MeiZhou", "ShanWei", "HeYuan", "YangJiang", "QingYuan", "DongGuan", "ZhongShan", "ChaoZhou", "JieYang", "YunFu"},
+    {"GuangXi", "NanNing", "LiuZhou", "GuiLin", "WuZhou", "BeiHai", "FangChengGang", "QinZhou", "GuiGang", "YuLin", "BaiSe", "HeZhou", "HeChi", "LaiBin", "ChongZuo"},
+    {"HaiNan", "HaiKou", "SanYa", "SanSha", "DanZhou"},
+    {"SiChuan", "ChengDu", "ZiGong", "PanZhiHua", "LuZhou", "DeYang", "MianYang", "GuangYuan", "SuiNing", "NeiNiang", "LeShan", "NanChong", "MeiShan", "YiBin", "GuangAn", "DaZhou", "YaAn", "BaZhong", "ZiYang"},
+    {"GuiZhou", "GuiYang", "LiuPanShui", "ZunYi", "AnShun", "BiJie", "TongRen", "QianXiNan", "QianDongNan", "QianNan"},
+    {"YunNan", "KunMing", "QuJing", "YuXi", "BaoShan", "ZhaoTong", "LiJiang", "PuEr", "LinCang"},
+    {"XiZang", "LaSa", "RiKaZe", "CangDu", "LinZhi", "ShanNan", "NaQu"},
+    {"ShanXi", "XiAn", "TongChuan", "BaoJi", "XianYang", "WeiNan", "YanAn", "HanZhong", "YuLin", "AnKang", "ShangLuo"},
+    {"GanSu", "LanZhou", "JiaYuGuan", "JinChang", "BaiYin", "TianShui", "WuWei", "ZhangYe", "PingLiang", "JiuQuan", "QingYang", "DingXi", "LongNan"},
+    {"QingHai", "XiNing", "HaiDong"},
+    {"NingXia", "YinChuan", "ShiZuiShan", "WuZhong", "GuYuan", "ZhongWei"},
+    {"XinJiang", "WuLuMuQi", "KeLaMaYi", "TuLuFan", "HaMi"},
+    {"TaiwanSheng", "TaiBei", "GaoXiong", "TaiZhong", "TaiNan", "XinBei", "TaoYuan"},
+    {"XiangGang"},
+    {"AoMen"}
+};
+
+#endif // PROVINCE_CITIES_H
